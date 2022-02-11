4cedb32b7a053884e960277ee3ca9ca5069e457d
6epotato <aarifurrahman93@gmail.com>
2022-02-12 00:20:05 +0600

    diposit porjonto

--- js/bank.js
+++ js/bank.js
@@ -1,6 +1,20 @@
 document.getElementById('login-submit').addEventListener('click', function () {
+
+    //get user email
+
     const emailField = document.getElementById('user-email');
     const userEmail = emailField.value;
-    console.log(userEmail);
+
+    // get user password
+
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+
+    //check user email and password
+
+    if (userEmail == 'abc@mail.com' && userPassword == '1234') {
+        window.location.href = 'banking.html';
+    }
+
 
 })
